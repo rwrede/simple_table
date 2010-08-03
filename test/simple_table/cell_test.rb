64f4c6d9f775d4e808357b2d6f756f1a8340d878
Sven Fuchs <svenfuchs@artweb-design.de>
2010-08-03 09:05:27 +0200

    refactor cell_test

--- test/simple_table/cell_test.rb
+++ test/simple_table/cell_test.rb
@@ -3,21 +3,26 @@ require File.dirname(__FILE__) + "/../test_helper"
 module SimpleTable
   class CellTest < Test::Unit::TestCase
     def test_render
-      html = Cell.new(Row.new(Table.new), 'foo').render
-      assert_html html, 'td', 'foo'
+      assert_html render_table_cell('foo'), 'td', 'foo'
     end
 
     def test_picks_th_when_contained_in_head
-      html = Cell.new(Row.new(Head.new(Table.new)), 'foo').render
-      assert_html html, 'th', 'foo'
+      assert_html render_head_cell('foo'), 'th', 'foo'
     end
 
-    def test_cell_contents_are_escaped
-      html = Cell.new(Row.new(Table.new), '<script>').render
-      assert_html html, 'td', '&lt;script&gt;'
-
-      html = Cell.new(Row.new(Head.new(Table.new)), '<script>').render
-      assert_html html, 'th', '&lt;script&gt;'
+    def test_cell_contents_are_xss_protected
+      assert_html render_table_cell('<script>'), 'td', '&lt;script&gt;'
+      assert_html render_head_cell('<script>'),  'th', '&lt;script&gt;'
     end
+
+    protected
+
+      def render_table_cell(content)
+        Cell.new(Row.new(Table.new), content).render
+      end
+
+      def render_head_cell(content)
+        Cell.new(Row.new(Head.new(Table.new)), content).render
+      end
   end
 end
